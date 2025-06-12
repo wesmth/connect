d4d7b08a5c4714405219f754883acab140e976b7
Weslen Matheus <wesmth@gmail.com>
2025-06-11 21:10:37 -0300

    Resolvendo bug do erro 500 no celular

--- question/views.py
+++ question/views.py
@@ -1,35 +1,26 @@
 from django.shortcuts import render, redirect
 import requests
 
-# View responsável por exibir e processar o formulário de pergunta
 def make_question(request):
-    # Verifica se a requisição é do tipo POST (ou seja, o form foi enviado)
     if request.method == 'POST':
-        # Pega o valor digitado no campo "make_question"
         question = request.POST.get('make_question')
-        # Salva essa pergunta na sessão pra usar depois
         request.session['question'] = question
-        # Redireciona pra mesma view (pra evitar reenvio do form ao atualizar a página)
         return redirect('make_question')
 
-    # Tenta recuperar a pergunta salva na sessão
     question = request.session.get('question')
 
-    # Inicializa as variáveis que vão conter a resposta e imagem da API
     answer = None
     answer_image = None
 
-    # URL da API que responde sim/não/talvez
     url = 'https://yesno.wtf/api'
 
-    # Se tiver uma pergunta na sessão, faz a requisição
     if question:
         try:
-            # Faz a requisição pra API e já converte em JSON
-            response = requests.get(url).json()
+            response = requests.get(url)
+            response.raise_for_status()  
+            data = response.json()
 
-            # Pega a resposta e traduz pra português
-            answer = response['answer']
+            answer = data.get('answer')
             if answer == 'yes':
                 answer = 'Sim'
             elif answer == 'no':
@@ -37,24 +28,21 @@ def make_question(request):
             else:
                 answer = 'Talvez'
 
-            # Pega a URL da imagem associada à resposta
-            answer_image = response['image']
+            answer_image = data.get('image')
 
-            print('Conexão bem sucedida.')
+            if 'question' in request.session:
+                del request.session['question']
+
+        except requests.RequestException as error:
+            print('Falha na conexão:', error)
+            answer = 'Erro na API'
+            answer_image = None
             
-            # Limpa a pergunta da sessão pra não exibir de novo ao recarregar a página
-            del request.session['question']
 
-        # Se der ruim na requisição, exibe erro no terminal
-        except (requests.RequestException) as error:
-            print('Falha na conexão', error)
-
-    # Cria o dicionário com os dados pra passar pro template
     context = {
         'answer': answer,
         'answer_image': answer_image,
         'question': question
     }
 
-    # Renderiza a página com as informações
     return render(request, 'question/question_page.html', context)
